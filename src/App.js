7b4bd2177cf32057964c17f3717c507dde38aac3
Prince kunzwa <princekunzwa@outlook.com>
2021-11-01 02:15:45 +0000

    API tests push

--- src/App.js
+++ src/App.js
@@ -3,40 +3,64 @@ import Movie from './Movie'
 import { useEffect, useState } from 'react';
 import './App.css';
 
+
+
 const App = () => {
 
-  const[movie, setMovie] = useState([]);
+  const[movies, setMovies] = useState([]);
+  const[search, setSearch] = useState("");
+
+  // only updates after submit is clicked
+  const[query, setQuery] = useState('Film');
 
 
-  useEffect( () => {
+  useEffect(() => {
     getMovies();
-  }, []);
+    console.log('lets say we are fetching');
+  }, [query]);
 
+
+  // Fetching the API and assigning to getMovies
   const getMovies = async () => {
-    const response = await fetch('https://api.themoviedb.org/3/movie/550?api_key=f2360989338abcdd53ef13d0fa0b762a');
+    const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=f2360989338abcdd53ef13d0fa0b762a&language=en-US&page=1');
     const data = await response.json();
-    console.log(data);
-    
+    console.log(data.results);
+  };
+
+
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
+  //stopping the constant updating
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
   }
+
+
+
 
   return(
     <div className="App">
-     <form className="search-form">
-       <input className="search-bar" type="text"/>
+      <h1>Movie Database</h1>
+     <form onSubmit={getSearch} className="search-form">
+       <input className="search-bar" type="text" value={search} onChange={updateSearch} />
        <button className="search-button" type="submit">
          Search
          </button>
      </form>
 
-    {movie.map(getMovies => (
+    {movies.map(movies => (
       <Movie 
-      title={getMovies.data.title} 
-      tagline={getMovies.tagline} 
-      image={getMovies.data.poster_path}
+      title={setMovies.title} 
+      tagline={setMovies.tagline} 
+      image={setMovies.data}
       />
     ))}
 
-     <h1>Hello worlhgfd</h1>
     </div>
   )
 }
